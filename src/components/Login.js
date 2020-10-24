7c8c11776d3a737396b556c1d299b116e12d0969
Szpoti <gtagigafan@gmail.com>
2020-10-24 12:27:24 +0200

    Login screen added

---
+++ src/components/Login.js
@@ -0,0 +1,91 @@
+import React, { useState } from "react";
+import { Container, Form, Button, Alert } from "react-bootstrap";
+import Password from "./InputComponents/Password";
+import Email from "./InputComponents/Email";
+import ValidationService from "../services/ValidationService";
+import { Link, Redirect, useHistory } from "react-router-dom";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessages, setErrorMessages] = useState([]);
+
+  const form = React.createRef();
+  const validator = new ValidationService();
+  const history = useHistory();
+
+  const asError = (message) => {
+    setErrorMessages((errorMessages) => [...errorMessages, message]);
+  };
+
+  const isFormValid = () => {
+    let isValid = true;
+    if (!validator.isValidEmail(email)) {
+      asError("Kérjük adjon meg egy érvényes email címet!");
+      isValid = false;
+    }
+
+    if (!validator.isValidPassword(password)) {
+      asError(
+        "A jelszónak tartalmaznia kell legalább egy számot, egy nagy és egy kisbetűt, és egy speciális karaktert ezek közül: !@#$%&*()-+=^"
+      );
+      isValid = false;
+    }
+
+    return isValid;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrorMessages([]);
+    if (!form.current.reportValidity()) {
+      return;
+    }
+
+    if (!isFormValid()) {
+      return false;
+    }
+
+    history.push("/products");
+  };
+
+  return (
+    <Container className="content-container">
+      <p className="h2">Bejelentkezés</p>
+
+      {errorMessages.map((message, index) => (
+        <Alert
+          key={`loginError-${index}`}
+          variant="danger"
+          className="text-center text-lg-left"
+        >
+          {message}
+        </Alert>
+      ))}
+
+      <Form ref={form}>
+        <Email setEmail={setEmail} />
+        <Password setPassword={setPassword} />
+        <Button
+          variant="primary"
+          type="submit"
+          className="mb-2 mr-sm-2"
+          onClick={handleSubmit}
+        >
+          Bejelentkezés
+        </Button>
+        <Button
+          className="mb-2 mr-sm-2"
+          onClick={() => history.push("/products")}
+        >
+          Tovább
+        </Button>
+      </Form>
+      <p>
+        Még nem regisztráltál? Tedd meg <Link to="/registration">itt!</Link>
+      </p>
+    </Container>
+  );
+};
+
+export default Login;
